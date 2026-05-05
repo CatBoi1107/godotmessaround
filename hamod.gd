657bcfadc934c691b9ed2f18eeda13abb26a7e39
CatBoi1107 <therealjohnsmith1256@gmail.com>
2026-05-05 07:31:16 +0700

    sprite ahh

--- hamod.gd
+++ hamod.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var AnimatedSprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var speed = 300.0
 @export var gravity = 4500
 const acceleration = 800.0
@@ -7,7 +8,15 @@ const deceleration = 500.0
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.ZERO
+	
+	var is_falling = velocity.y > 0 and not is_on_floor()
+	var is_idle = velocity == Vector2.ZERO
+	var is_moving = not is_idle
 
+	if is_idle:
+		AnimatedSprite.play("Idle")
+	else:
+		AnimatedSprite.play("Run")
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
@@ -42,6 +51,7 @@ func _physics_process(delta: float) -> void:
 	apply_floor_snap()
 
 func _ready():
+	AnimatedSprite.play("Idle")
 	var spawn_point = get_parent().get_node("SpawnPoint2D")
 	if spawn_point:
 		global_position = spawn_point.global_position
